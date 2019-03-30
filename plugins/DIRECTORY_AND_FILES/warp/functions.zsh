e6040c361cfa95b381430c1afe03cf6b208bbe75
Marcel Breyer <b-marci@web.de>
2019-03-30 16:06:19 +0100

    you can now directly cd to a directory for which a warp point exists, created README, updated completion function to support completing of existing warp points

--- plugins/DIRECTORY_AND_FILES/warp/functions.zsh
+++ plugins/DIRECTORY_AND_FILES/warp/functions.zsh
@@ -1,3 +1,11 @@
+# title           : warp/functions.zsh
+# description     : This file provides a function to directly cd to directories and helper functions to achiev this.
+# date            : 2019-03-30
+# dependencies    : -
+# zsh_version     : 5.7.1
+# ====================================================================================
+
+
 # easily read all lines of a file into an array
 zmodload zsh/mapfile
 
@@ -45,11 +53,13 @@ function _warp_check_absolute_path() {
 
 # add a new warp point if the name isn't taken yet and if it is a directory
 function _warp_add() {
-    local warp_point_exists=$(sed -n "/^$1=/p" $warp_file_dir)      ## check whether the given warp point name already exists
+    local warp_point_exists=$(sed -n "/^$1=/p" $warp_file_dir)
+    ## check whether the given warp point name already exists
     if [[ -z $warp_point_exists ]]; then
-        sed -i "$ a $1=$2" $warp_file_dir                           ## append new warp point after the last line
+        ## append new warp point after the last line
+        echo "$1=$2" >> $warp_file_dir
         echo "warp point added: $1 → $2"
-    else 
+    else
         echoerr "warp: $1 already added (use -! or --add! to override)"
         return 17
     fi
@@ -57,24 +67,30 @@ function _warp_add() {
 
 # add a new warp point or replace an already existing one
 function _warp_force_add() {
-    local warp_point_exists=$(sed -n "/^$1=/p" $warp_file_dir)      ## check whether the given warp point name already exists
+    local warp_point_exists=$(sed -n "/^$1=/p" $warp_file_dir)
+    ## check whether the given warp point name already exists
     if [[ -z $warp_point_exists ]]; then
-        sed -i "$ a $1=$2" $warp_file_dir                           ## append new warp point after the last line
+        ## append new warp point after the last line
+        echo "$1=$2" >> $warp_file_dir
         echo "warp point added: $1 → $2"
-    else 
-        sed -i "/^$1=/d" $warp_file_dir                             ## remove old warp point if neccessary
-        sed -i "$ a $1=$2" $warp_file_dir                           ## append updated warp point after the last line
+    else
+        ## remove old warp point if neccessary
+        sed -i "/^$1=/d" $warp_file_dir
+        ## append updated warp point after the last line
+        echo "$1=$2" >> $warp_file_dir
         echo "replaced warp point: $1: ${old_warp_dir#*=} ⇒ $2"
     fi
 }
 
 # remove the given warp point (if possible)
 function _warp_remove() {
-    local warp_point_exists=$(sed -n "/^$1=/p" $warp_file_dir)      ## check whether the given warp point name exists
-    if [[ -n $warp_point_exists ]]; then 
-       sed -i "/^$1=/d" $warp_file_dir                              ## remove warp point
+    local warp_point_exists=$(sed -n "/^$1=/p" $warp_file_dir)
+    ## check whether the given warp point name exists
+    if [[ -n $warp_point_exists ]]; then
+       ## remove warp point
+       sed -i "/^$1=/d" $warp_file_dir
        echo "warp point removed: ${warp_point_exists/=/ → }"
-    else 
+    else
        echoerr "warp: $1 doesn't exist (use -l or --list to list all available warp points)"
        return 2
     fi
@@ -82,13 +98,17 @@ function _warp_remove() {
 
 # remove all invalid warp points
 function _warp_clean() {
-    for line in "${(f)mapfile[$warp_file_dir]}"; do                 ## loop over all warp points
-        [[ ! $line =~ '[:space:]+' ]] && continue                   ## don't try to clean an empty line    
-        if ! [[ -d "${line#*=}" ]]; then                            ## check whether warp point directory still exists
-            sed -i "\|$line|d" $warp_file_dir                       ## remove warp point if directory is now invalid
+    ## loop over all warp points
+    for line in "${(f)mapfile[$warp_file_dir]}"; do
+        ## don't try to clean an empty line
+        [[ ! $line =~ '[:space:]+' ]] && continue
+        ## check whether warp point directory still exists
+        if ! [[ -d "${line#*=}" ]]; then
+            ## remove warp point if directory is now invalid
+            sed -i "\|$line|d" $warp_file_dir
             echo "removed invalid warp point: ${line/=/ → }"
         fi
-    done 
+    done
 }
 
 # list all warp points
@@ -105,7 +125,7 @@ Options:
     -!,  --add!       add a new warp point to the given directory with the specified name
                       overrides existing warp point if necessary
     -rm, --remove     removes an existing warp point
-    -c,  --clean      remove all unused warp points 
+    -c,  --clean      remove all unused warp points
     -l,  --list       lists all warp points
     -h,  --help       display help message
 Return Codes:
@@ -114,8 +134,25 @@ Return Codes:
     7       illegal number of parameters
     17      warp point already exists
     20      illegal warp point target path (not an absolute directory)
-    22      illegal warp point name 
+    22      illegal warp point name
 EOF
+}
+
+# try to warp
+function _warp_to_point() {
+  local warp_point_exists=$(sed -n "/^$1=/p" $warp_file_dir)
+  ## check whether the given warp point name exists
+  if [[ -n $warp_point_exists ]]; then
+    ## try to change to the directory saved in the given warp point
+    builtin cd "${warp_point_exists#*=}" >/dev/null 2>&1
+    ## warp point target doesn't exist anymore
+    if [[ $? != 0 ]]; then
+      echoerr "warp: no such directory: ${warp_point_exists#*=}"
+    fi
+  else
+    ## no warp point was given
+    echoerr "warp: not a warp point: $1"
+  fi
 }
 
 
@@ -130,8 +167,8 @@ function warp() {
     ## get user option and remove it from the positional parameters
     local option=$1
     shift
-    
-    case $option in 
+
+    case $option in
         ('--add'|'-a')
             ## if no directory is given use current working directory
             if [[ $# == 1 ]]; then
@@ -141,7 +178,7 @@ function warp() {
             _warp_check_parameter_count 2 $# && \
             _warp_check_illegal_name $1 && \
             _warp_check_absolute_path "$2" && \
-            _warp_add "$@" 
+            _warp_add "$@"
         ;;
         ('--add!'|'-!')
             ## if no directory is given use current working directory
@@ -173,9 +210,8 @@ function warp() {
             _warp_check_parameter_count 0 $# && _warp_help "$@"
         ;;
         (*)
-            ## illegal option
-            echoerr "$0: illegal option"
-            return 1
+            ## try to cd to the given warp point target
+            _warp_check_parameter_count 0 $# && _warp_to_point "$option"
         ;;
     esac
 }
